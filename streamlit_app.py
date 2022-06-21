2f67a0aba1308aeb61815009f07bda3395979f61
Frank Kienle <privat@kienlefrank.de>
2022-06-21 21:28:13 +0200

    Update streamlit_app.py
    Plotly example worldmap

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,48 +1,70 @@
-import plotly.figure_factory as ff
-import numpy as np
-
-
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
 
 import streamlit as st
 import pandas as pd
+import numpy as np
+
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from plotly import tools
+
 
 
 """
-# Welcome to Streamlit!
+# Welcome to Streamlit Example Covid cases worldmap!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+The code is deployed via Streamlit Cloud
+Explaining of the world map graphic can be found in the
+Udemy lecture: https://bit.ly/3sM0cYR
 """
 
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
+# GET DATA
+url="https://covid.ourworldindata.org/data/owid-covid-data.csv"
+df_country_info=pd.read_csv(url,sep=',')
 
-# Group data together
-hist_data = [x1, x2, x3]
 
-group_labels = ['Group 1', 'Group 2', 'Group 3']
+# cut out the last day with data
+df_country_info_day=df_country_info[df_country_info['date']==max(df_country_info['date'])]
 
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-         hist_data, group_labels, bin_size=[.1, .25, .5])
+## Visualize something
+#visualize_col='total_vaccinations_per_hundred'
+visualize_col='total_cases in %'
+
+my_z=df_country_info_day['total_cases_per_million']/10000
+
+fig = go.Figure(go.Choropleth(
+                    locations = df_country_info_day['iso_code'],
+                    z = my_z, # norm to percent
+                    text = df_country_info_day['location'],
+                    colorscale = 'Blues',
+                    autocolorscale=False,
+                    reversescale=False,
+                    marker_line_color='darkgray',
+                    marker_line_width=0.5,
+                    colorbar_title = visualize_col,
+                    ))
+
+# Beatify the graph_objects
+
+fig.update_layout(
+    #width=1200, height=900,
+    title_text='COVID-19 Total cases in %',
+    geo=dict(
+        showframe=False,
+        showcoastlines=False,
+        projection_type='equirectangular'
+    ),
+    annotations = [dict(
+        x=0.55,
+        y=0.1,
+        xref='paper',
+        yref='paper',
+        text='Data Source: <a href="https://github.com/owid/covid-19-data/tree/master/public/data/">\
+            Our World in Data</a>',
+        showarrow = False
+    )]
+)
+
+
 
 # Plot!
 st.plotly_chart(fig, use_container_width=True)
